37a882c6ade0b804e1cd51e0d8475c57a9e94fa8
Prashant Agrawal <prashant@enthought.com>
2013-07-09 18:47:27 +0530

    Cleanup mapping names. Add a method to register a trait_to_jigna mapping.

--- jigna/editors/factory_mapping.py
+++ jigna/editors/factory_mapping.py
@@ -9,34 +9,34 @@ from traitsui.api import TextEditor, RangeEditor, EnumEditor, ListEditor, Instan
 
 class FactoryMapping(object):
 
-    traitsui_to_jigna_mapping = {
-                                  TextEditor: JStringEditor, 
-                                  RangeEditor: JRangeEditor,
-                                  EnumEditor: JEnumEditor,
-                                  ListEditor: JListEditor,
-                                  InstanceEditor: JInstanceEditor,
-                                  BooleanEditor: JBoolEditor,
-                                }
+    traitsui_to_jigna_editor = {
+                          TextEditor: JStringEditor, 
+                          RangeEditor: JRangeEditor,
+                          EnumEditor: JEnumEditor,
+                          ListEditor: JListEditor,
+                          InstanceEditor: JInstanceEditor,
+                          BooleanEditor: JBoolEditor,
+                        }
 
-    trait_type_to_editor_factory_mapping = {
-                                         Int: {'simple': JIntEditor},
-                                         Str: {'simple': JStringEditor},
-                                         Float: {'simple': JFloatEditor},
-                                         Range: {'simple': JRangeEditor},
-                                         Bool: {'simple': JBoolEditor},
-                                         Enum: {'simple': JEnumEditor},
-                                         List: {'simple': JListEditor},
-                                         Instance: {'simple': JInstanceEditor}
-                                      }
+    trait_to_jigna_editor = {
+                       Int: {'simple': JIntEditor},
+                       Str: {'simple': JStringEditor},
+                       Float: {'simple': JFloatEditor},
+                       Range: {'simple': JRangeEditor},
+                       Bool: {'simple': JBoolEditor},
+                       Enum: {'simple': JEnumEditor},
+                       List: {'simple': JListEditor},
+                       Instance: {'simple': JInstanceEditor}
+                    }
 
     @classmethod
     def get_editor_factory(cls, obj, item):
         """ Get the Jigna editor factory for the given traitsui `Item` object.
         """
-        editor_factory = cls.traitsui_to_jigna_mapping.get(item.editor.__class__)
+        editor_factory = cls.traitsui_to_jigna_editor.get(item.editor.__class__)
         if not editor_factory:
             trait_type = obj.trait(item.name).trait_type.__class__
-            editor_factory_dict = cls.trait_type_to_editor_factory_mapping.get(trait_type)
+            editor_factory_dict = cls.trait_to_jigna_editor.get(trait_type)
             if editor_factory_dict:
                 return editor_factory_dict.get(item.style or 'simple')
             else:
@@ -45,28 +45,48 @@ class FactoryMapping(object):
         return editor_factory
 
     @classmethod
-    def register_editor_factory(cls, traitsui_factory, jigna_factory):
+    def register_traitsui_mapping(cls, traitsui_factory, jigna_factory):
         """ Register the mapping between the given traitsui editor factory and the 
         corresponding Jigna editor factory. 
 
         This means that a traitsui item edited with an editor of class `traitsui_factory`, 
         it will be shown in HTML using an editor of class `jigna_factory`
         """
-        cls.traitsui_to_jigna_mapping[traitsui_factory] = jigna_factory
+        cls.traitsui_to_jigna_editor[traitsui_factory] = jigna_factory
 
-def register_additional_factories():
+    @classmethod
+    def register_trait_mapping(self, trait_type, jigna_factory, style='simple'):
+        """ Register the mapping between the given traitsui editor factory and the 
+        corresponding Jigna editor factory. 
+
+        This means that a trait of type `trait_type` will be edited with `jigna_factory`
+        unless another editor is specified explicitly.
+        """
+        jigna_factory_dict = cls.trait_to_jigna_editor.get(trait_type, None)
+        if not jigna_factory_dict:
+            jigna_factory_dict = {style: jigna_factory}
+        else:
+            jigna_factory_dict[style] = jigna_factory
+        cls.trait_to_jigna_editor[trait_type] = jigna_factory_dict
+
+
+def register_extra_traitsui_mappings():
     try:
         from mayavi.core.ui.api import SceneEditor
         from jigna.editors.api import MayaviPlotEditor
-        FactoryMapping.register_editor_factory(SceneEditor, MayaviPlotEditor)
+        FactoryMapping.register_traitsui_mapping(SceneEditor, MayaviPlotEditor)
     except ImportError:
         pass
 
     try:
         from enable.api import ComponentEditor
         from jigna.editors.api import ChacoPlotEditor
-        FactoryMapping.register_editor_factory(ComponentEditor, ChacoPlotEditor)
+        FactoryMapping.register_traitsui_mapping(ComponentEditor, ChacoPlotEditor)
     except ImportError:
         pass
 
-register_additional_factories()
+def register_extra_trait_mappings():
+    pass
+
+register_extra_traitsui_mappings()
+register_extra_trait_mappings()
