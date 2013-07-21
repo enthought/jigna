8d90da9770b112389c6c1e6f155cc3f24753bdda
Prabhu Ramachandran <prabhu@enthought.com>
2013-07-22 01:00:33 +0530

    Make mayavi and chaco imports conditional.
    This makes it so we do not need mayavi or chaco installed to use Jigna.

--- jigna/editors/mayavi_editors.py
+++ jigna/editors/mayavi_editors.py
@@ -1,6 +1,5 @@
 # Enthought library imports
 from traitsui.api import View, Item
-from mayavi.core.ui.api import MayaviScene, SceneEditor
 
 from traitsui_editor import TraitsUIWidget, TraitsUIEditor
 
@@ -11,6 +10,7 @@ class MayaviPlotEditor(TraitsUIEditor):
 
     def create_widget(self, model_id, args):
         width, height = self.get_size(args)
+        from mayavi.core.ui.api import MayaviScene, SceneEditor
         view = View(Item(self.tname, show_label=False,
                          editor=SceneEditor(scene_class=MayaviScene)),
                          width=width, height=height, resizable=True)
