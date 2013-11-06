945240b1aa0c641e1cb9ed30238b27f013cb255d
Martin Chilvers <martin.chilvers@gmail.com>
2013-11-06 14:31:07 +0000

    removed guard for creating new proxies on object change

--- jigna/resources/js/jigna.js
+++ jigna/resources/js/jigna.js
@@ -229,9 +229,7 @@ jigna.Broker.prototype._on_object_changed = function(event) {
     // fixme: This smells... It is used to recreate a list proxy every time
     // a list changes but that blows away caching advantages. Can we make it
     // smarter by managing the details of a TraitListEvent?
-    if (event.new_obj.type === 'list') {
-        this._create_proxy(event.new_obj.type, event.new_obj.value);
-    }
+    this._create_proxy(event.new_obj.type, event.new_obj.value);
 
     if (this._scope.$$phase === null){
         this._scope.$digest();
