ea2bd2680b5bfb5d23f63ef5a3a8013020167daf
Prashant Agrawal <prashant@enthought.com>
2013-11-07 23:19:10 +0530

    update person_example_full_html to show the ability to pass multiple models.

--- examples/person_example_full_html.py
+++ examples/person_example_full_html.py
@@ -20,8 +20,11 @@ html = """
         </head>
 
         <body>
-            Name: <input ng-model="model.name">
-            Age: <input ng-model="model.age" type='number'>
+            Name: <input ng-model="fred.name">
+            Age: <input ng-model="fred.age" type='number'>
+
+            Name: <input ng-model="wilma.name">
+            Age: <input ng-model="wilma.age" type='number'>
         </body>
     </html>
 """
@@ -35,17 +38,26 @@ def main():
         print obj, traitname, old, new
 
     fred  = Person(name='Fred', age=42)
+    wilma = Person(name='Wilma', age=4)
+    
     fred.on_trait_change(listener)
     def update_fred():
-        fred.name = "Wilma"
-        fred.age = 4
+        fred.name = "Freddie"
+        fred.age = 43
+
+    def update_wilma():
+        wilma.name = "Mrs. Wilma"
+        wilma.age = 25
 
     app = QtGui.QApplication.instance() or QtGui.QApplication([])
-    person_view.show(model=fred)
+    person_view.show(fred=fred, wilma=wilma)
     do_after(2000, update_fred)
+    do_after(3000, update_wilma)
     app.exec_()
     print fred.name
     print fred.age
+    print wilma.name
+    print wilma.age
 
 if __name__ == "__main__":
     main()
