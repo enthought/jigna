884dac797cc2385c8d62d4e07fe3815f3b2c5d57
Martin Chilvers <martin.chilvers@gmail.com>
2013-11-26 11:33:02 +0000

    fixed tests to not reply on the angular scope

--- jigna/tests/test_jigna_qt.py
+++ jigna/tests/test_jigna_qt.py
@@ -71,7 +71,6 @@ class TestJignaQt(unittest.TestCase):
         self.fred.friends = []
 
     def execute_js(self, js):
-        js = 'jigna.client.js_framework.scope.' + js
         GUI.process_events()
         result = self.bridge.widget.execute_js(js)
         GUI.process_events()
@@ -95,75 +94,81 @@ class TestJignaQt(unittest.TestCase):
     def test_simple_primitive_traits(self):
         fred = self.fred
         fred.name = "Freddie"
-        self.assertJSEqual("model.name", fred.name)
+        self.assertJSEqual("jigna.models.model.name", fred.name)
         fred.age = 43
-        self.assertJSEqual("model.age", fred.age)
+        self.assertJSEqual("jigna.models.model.age", fred.age)
 
     def test_list_of_primitives(self):
-        self.assertJSEqual("model.fruits", [])
+        self.assertJSEqual("jigna.models.model.fruits", [])
         fred = self.fred
         fred.fruits = ["banana", "mango"]
-        self.assertJSEqual("model.fruits", fred.fruits)
+        self.assertJSEqual("jigna.models.model.fruits", fred.fruits)
 
         # Now set the value in the JS side.
-        self.execute_js("model.fruits[0] = 'peach'")
+        self.execute_js("jigna.models.model.fruits[0] = 'peach'")
         self.assertEqual(fred.fruits, ["peach", "mango"])
 
-        self.execute_js("model.fruits = ['apple']")
+        self.execute_js("jigna.models.model.fruits = ['apple']")
         self.assertEqual(fred.fruits, ["apple"])
 
     def test_dict_of_primitives(self):
-        self.assertJSEqual("model.phonebook", {})
+        self.assertJSEqual("jigna.models.model.phonebook", {})
         fred = self.fred
         fred.phonebook = {'joe' : 123, 'joan' : 345}
-        self.assertJSEqual("model.phonebook", fred.phonebook)
+        self.assertJSEqual("jigna.models.model.phonebook", fred.phonebook)
 
         # Now set the value in the JS side.
-        self.execute_js("model.phonebook['joe'] = 567")
+        self.execute_js("jigna.models.model.phonebook['joe'] = 567")
         self.assertEqual(567, fred.phonebook['joe'])
 
-        self.execute_js("model.phonebook = {'alan' : 987}")
+        self.execute_js("jigna.models.model.phonebook = {'alan' : 987}")
         self.assertEqual(fred.phonebook, {'alan' : 987})
 
     def test_instance_trait(self):
-        self.assertJSEqual("model.spouse", '')
+        self.assertJSEqual("jigna.models.model.spouse", '')
         wilma = Person(name='Wilma', age=40)
         self.fred.spouse = wilma
-        self.assertJSEqual("model.spouse.name", 'Wilma')
-        self.assertJSEqual("model.spouse.age", 40)
+        self.assertJSEqual("jigna.models.model.spouse.name", 'Wilma')
+        self.assertJSEqual("jigna.models.model.spouse.age", 40)
 
         # Set in the JS side.
-        self.execute_js("model.spouse.name = 'Wilmaji'")
-        self.execute_js("model.spouse.age = 41")
+        self.execute_js("jigna.models.model.spouse.name = 'Wilmaji'")
+        self.execute_js("jigna.models.model.spouse.age = 41")
         self.assertEqual(wilma.name, "Wilmaji")
         self.assertEqual(wilma.age, 41)
 
     def test_list_instance(self):
-        self.assertJSEqual("model.friends", [])
+        self.assertJSEqual("jigna.models.model.friends", [])
         barney = Person(name="Barney", age=40)
         fred = self.fred
         fred.friends = [barney]
-        self.assertJSEqual("model.friends[0].name", "Barney")
-        self.assertJSEqual("model.friends[0].age", 40)
+        self.assertJSEqual("jigna.models.model.friends[0].name", "Barney")
+        self.assertJSEqual("jigna.models.model.friends[0].age", 40)
 
         dino = Person(name="Dino", age=10)
         fred.friends.append(dino)
-        self.assertJSEqual("model.friends[1].name", "Dino")
-        self.assertJSEqual("model.friends[1].age", 10)
-        self.execute_js("model.friends[0].name = 'Barneyji'")
+        self.assertJSEqual("jigna.models.model.friends[1].name", "Dino")
+        self.assertJSEqual("jigna.models.model.friends[1].age", 10)
+        self.execute_js("jigna.models.model.friends[0].name = 'Barneyji'")
         self.assertEqual(barney.name, "Barneyji")
 
     def test_list_sortable(self):
         # Given
         fred = self.fred
-        self.execute_js("model.fruits = ['peach', 'apple', 'banana']")
-        self.assertJSEqual("model.fruits", ['peach', 'apple', 'banana'])
+        self.execute_js(
+            "jigna.models.model.fruits = ['peach', 'apple', 'banana']"
+        )
+        self.assertJSEqual(
+            "jigna.models.model.fruits", ['peach', 'apple', 'banana']
+        )
 
         # When
-        self.execute_js("model.fruits.sort()")
+        self.execute_js("jigna.models.model.fruits.sort()")
 
         # Then
-        self.assertJSEqual("model.fruits", ['apple', 'banana', 'peach'])
+        self.assertJSEqual(
+            "jigna.models.model.fruits", ['apple', 'banana', 'peach']
+        )
         self.assertEqual(fred.fruits, ['apple', 'banana', 'peach'])
 
 
@@ -171,15 +176,15 @@ class TestJignaQt(unittest.TestCase):
         fred = self.fred
         wilma = Person(name='Wilma', age=40)
         self.fred.spouse = wilma
-        self.execute_js("model.method('hello')")
+        self.execute_js("jigna.models.model.method('hello')")
         self.assertEqual(fred.called_with, "hello")
-        self.execute_js("model.method(1)")
+        self.execute_js("jigna.models.model.method(1)")
         self.assertEqual(fred.called_with, 1)
-        self.execute_js("model.method(10.0)")
+        self.execute_js("jigna.models.model.method(10.0)")
         self.assertEqual(fred.called_with, 10.0)
-        self.execute_js("model.method([1, 2])")
+        self.execute_js("jigna.models.model.method([1, 2])")
         self.assertEqual(fred.called_with, [1,2])
-        self.execute_js("model.method(jigna.client.js_framework.scope.model.spouse)")
+        self.execute_js("jigna.models.model.method(jigna.models.model.spouse)")
         self.assertEqual(fred.called_with, wilma)
 
 if __name__ == "__main__":
