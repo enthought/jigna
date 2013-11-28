507025d9f99a9ce4a8130c25020635d9fc6b18ec
Prashant Agrawal <prashant@enthought.com>
2013-11-28 09:45:17 +0530

    Minor cleanup.

--- jigna/qt_server.py
+++ jigna/qt_server.py
@@ -55,20 +55,18 @@ class QtServer(Server):
 
         self._bridge = QtBridge(widget=widget, server=self)
 
-        widget.callbacks = [('handle_request', self._bridge.handle_request)]
-
-        widget.python_namespace = 'qt_bridge'
+        widget.trait_set(
+            callbacks = [('handle_request', self._bridge.handle_request)],
+            python_namespace = 'qt_bridge',
+            root_paths = {
+                'jigna': FileLoader(
+                    root = join(abspath(dirname(__file__)), 'resources')
+                )
+            },
+            open_externally = True,
+            debug = True                        
+        )
         
-        widget.root_paths = {
-            'jigna': FileLoader(
-                root = join(abspath(dirname(__file__)), 'resources')
-            )
-        }
-        
-        widget.open_externally = True
-        
-        widget.debug = True
-
         widget.create()
 
         widget.load_html(self.html, self.base_url)
