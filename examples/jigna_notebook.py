9d95eb683829502accb48f8036eea25f08051c6e
Prabhu Ramachandran <prabhu@enthought.com>
2013-12-25 15:18:43 +0530

    NEW: Example IPython notebook + jigna integration.
    This allows us to use jigna from an IPython notebook.  The notebook
    server needs to be modified to proxy the jigna get requests.  To run the
    example simply run python jigna_notebook.py and open the notebook shown
    on your browser and then run the cells.

---
+++ examples/jigna_notebook.py
@@ -0,0 +1,70 @@
+from IPython.display import HTML
+from textwrap import dedent
+
+
+class JignaNotebook(object):
+    def __init__(self, port=9999):
+        self.server_thread = None
+        self.port = port
+        self._code_count = 0
+
+    def start_server(self):
+        from jigna.api import View
+        view = View(body_html='')
+        self.view = view
+        from threading import Thread
+        t = Thread(target=lambda: view.serve(port=self.port))
+        t.daemon = True
+        t.start()
+        self.server_thread = t
+
+    def add_models(self, **context):
+        self.view.update_context(**context)
+
+    def get_ipython_html(self, body_html):
+        server = "localhost:%d"%self.port
+        div_id = 'injected%d'%self._code_count
+        self._code_count += 1
+        src = dedent("""
+            <div id="{div_id}">
+                {body_html}
+            </div>
+            <script>
+            try {{
+                jigna
+            }}
+            catch(err)
+            {{
+                window.jigna_server = "{server}";
+                $.getScript("http://{server}/jigna/js/angular.min.js", function() {{
+                    $.getScript("http://{server}/jigna/js/jigna.js", function() {{
+                        angular.bootstrap(document, ['jigna']);
+                        console.log("Started jigna.");
+                    }})
+                }});
+            }} finally {{
+                setTimeout(function() {{
+                    jigna.recompile($("#{div_id}"));
+                }}, 0);
+            }}
+            </script>
+              """.format(div_id=div_id, body_html=body_html, server=server))
+        return src
+
+    def show(self, body_html, **context):
+        if len(context) > 0:
+            self.add_models(**context)
+        return HTML(self.get_ipython_html(body_html))
+
+
+
+def main():
+    import _jigna_notebook_app
+    _jigna_notebook_app.main()
+
+if __name__ == '__main__':
+    main()
+else:
+    jigna_nb = JignaNotebook()
+    jigna_nb.start_server()
+    show = jigna_nb.show
