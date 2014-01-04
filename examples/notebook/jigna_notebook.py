308bd9e5ae9a2cbb5a2663e13f1c946848bd58ba
Prashant Agrawal <prashant@enthought.com>
2014-01-05 03:13:24 +0530

    Some clean up.

--- examples/notebook/jigna_notebook.py
+++ examples/notebook/jigna_notebook.py
@@ -1,12 +1,28 @@
 from IPython.display import HTML
-from textwrap import dedent
 
+HTML_TEMPLATE = """
+<div id="{div_id}">
+    {html}
+</div>
+<script>
+    $.getScript("http://{server}/jigna/js/angular.min.js", function() {{
+        $.getScript("http://{server}/jigna/js/jigna.js", function() {{
+            var elem = $("#{div_id}");
+
+            elem.ready(function(){{
+                angular.bootstrap(elem, ['jigna']);
+            }})
+        }})
+    }})
+</script>
+"""
 
 class JignaNotebook(object):
     def __init__(self, port=9999):
         self.server_thread = None
         self.port = port
         self._code_count = 0
+        self.start_server()
 
     def start_server(self):
         from jigna.api import View
@@ -18,37 +34,25 @@ class JignaNotebook(object):
         t.start()
         self.server_thread = t
 
-    def add_models(self, **context):
-        self.view.update_context(**context)
+    def show(self, html, **context):
+        if len(context) > 0:
+            self.view.update_context(**context)
+        
+        return HTML(self._get_html(html))
 
-    def get_ipython_html(self, body_html):
+    #### Private protocol #####################################################
+
+    def _get_html(self, html):
         server = "localhost:%d"%self.port
         div_id = 'injected%d'%self._code_count
         self._code_count += 1
-        src = dedent("""
-            <div id="{div_id}">
-                {body_html}
-            </div>
-            <script>
-            $.getScript("http://{server}/jigna/js/angular.min.js", function() {{
-                $.getScript("http://{server}/jigna/js/jigna.js", function() {{
-                    var elem = $("#{div_id}");
 
-                    elem.ready(function(){{
-                        angular.bootstrap(elem, ['jigna']);
-                    }})
-                }})
-            }})
-            </script>
-              """.format(div_id=div_id, body_html=body_html, server=server))
+        src = HTML_TEMPLATE.format(
+            div_id = div_id,
+            html = html,
+            server = server
+        )
         return src
-
-    def show(self, body_html, **context):
-        if len(context) > 0:
-            self.add_models(**context)
-        return HTML(self.get_ipython_html(body_html))
-
-
 
 def main():
     import _jigna_notebook_app
@@ -56,7 +60,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-else:
-    jigna_nb = JignaNotebook()
-    jigna_nb.start_server()
-    show = jigna_nb.show
