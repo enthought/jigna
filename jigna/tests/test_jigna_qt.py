068592c7132854a23a7bf9dbd730c21289678a9d
Prashant Agrawal <prashant@enthought.com>
2014-01-05 21:36:42 +0530

    Add back the assert statement which  tests dynamic addition of models to scope.

--- jigna/tests/test_jigna_qt.py
+++ jigna/tests/test_jigna_qt.py
@@ -195,6 +195,7 @@ class TestJignaQt(unittest.TestCase):
         self.assertJSEqual("jigna.models.new_model", None)
         self.person_view.update_context(new_model=new_model)
         self.assertJSEqual("jigna.models.new_model.name", new_model.name)
+        self.assertJSEqual("$(document.body).scope().new_model.name", new_model.name)
         
 if __name__ == "__main__":
     unittest.main()
