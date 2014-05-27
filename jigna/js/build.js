77fcae026d3453c5881b101fac7b8afd558778b0
Prashant Agrawal <prashant@enthought.com>
2014-05-27 18:32:44 +0530

    Add the ability to add a dist compiled version of jigna.js

---
+++ jigna/js/build.js
@@ -0,0 +1,8 @@
+{
+    "baseUrl": ".",
+    "name": "external/almond",
+    "include": ["main"],
+    "mainConfigFile": "main.js",
+    "out": "dist/main.js",
+    "wrap": true
+}
--- jigna/js/main.js
+++ jigna/js/main.js
@@ -1,6 +1,28 @@
-require(['angular', 'jigna-angular'], function(angular, jigna_app){
-    // Bootstrap the jigna-angular application when the document is ready
-    $(document).ready(function(){
-        angular.bootstrap(document, [jigna_app.name]);
-    });
+require.config({
+    baseUrl: '/jigna/',
+
+    paths: {
+        'jquery': 'external/jquery.min',
+        'angular': 'external/angular.min',
+        'jigna': 'app/jigna',
+        'jigna-angular': 'app/jigna-angular'
+    },
+
+    shim: {
+        'jquery': {
+            exports: 'jQuery'
+        },
+
+        'angular': {
+            exports: 'angular',
+            deps: ['jquery']
+        },
+
+        'jigna': {
+            exports: 'jigna',
+            deps: ['angular']
+        }
+    }
 });
+
+require(['jigna-angular']);
