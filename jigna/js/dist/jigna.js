4a5a008238cefd5f14960135137f6a458ff256e9
Prashant Agrawal <prashant@enthought.com>
2014-06-10 10:32:25 +0530

    WIP: Add the AsyncClient.
    TODO:
    1. Add async method on the bridge.
    2. Fix get_attribute_from_server for AsyncClient.
    3. Add an example to test async.

--- jigna/js/dist/jigna.js
+++ jigna/js/dist/jigna.js
@@ -958,10 +958,18 @@ define('jigna',['jquery', 'event_target', 'subarray', 'qt_bridge', 'web_bridge']
     // Namespace for all Jigna-related objects.
     var jigna = new EventTarget();
 
-    jigna.initialize = function() {
+    jigna.initialize = function(options) {
+
+        options = options || {};
 
         this.models = {};
-        this.client = new jigna.Client();
+
+        if (options.async) {
+            this.client = new jigna.AsyncClient();
+        }
+        else {
+            this.client = new jigna.Client();
+        }
 
         this.client.initialize();
     };
@@ -1116,6 +1124,17 @@ define('jigna',['jquery', 'event_target', 'subarray', 'qt_bridge', 'web_bridge']
         return deferred.promise();
     };
 
+    jigna.Client.prototype.get_attribute_from_server = function(proxy, attribute) {
+        /* Get the specified attribute of the proxy from the server. */
+
+        var request = this._create_request(proxy, attribute);
+
+        var response = this.send_request(request);
+        var result = this._unmarshal(response);
+
+        return result;
+    };
+
     jigna.Client.prototype.get_attribute = function(proxy, attribute) {
         /* Get the specified attribute of the proxy. If a cached value is
         available, return that; otherwise get it from the server. */
@@ -1132,17 +1151,6 @@ define('jigna',['jquery', 'event_target', 'subarray', 'qt_bridge', 'web_bridge']
         }
 
         return value;
-    };
-
-    jigna.Client.prototype.get_attribute_from_server = function(proxy, attribute) {
-        /* Get the specified attribute of the proxy from the server. */
-
-        var request = this._create_request(proxy, attribute);
-
-        var response = this.send_request(request);
-        var result = this._unmarshal(response);
-
-        return result;
     };
 
     jigna.Client.prototype.set_instance_attribute = function(id, attribute_name, value) {
@@ -1295,6 +1303,99 @@ define('jigna',['jquery', 'event_target', 'subarray', 'qt_bridge', 'web_bridge']
                 return value;
             }
         }
+    };
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // AsyncClient
+    ///////////////////////////////////////////////////////////////////////////////
+
+    jigna.AsyncClient = function() {};
+    jigna.AsyncClient.prototype = jigna.Client;
+
+    jigna.AsyncClient.prototype.send_request = function(request) {
+        /* Send a request to the server and wait for (and return) the response. */
+
+        var jsonized_request  = JSON.stringify(request);
+
+        var deferred = new $.Deferred();
+        this.bridge.send_request_async(jsonized_request).done(function(jsonized_response){
+            deferred.resolve(JSON.parse(jsonized_response).result);
+        });
+
+        return deferred.promise();
+    };
+
+    jigna.AsyncClient.prototype.call_instance_method = function(id, method_name, args) {
+        /* Calls an instance method. Do not use this to call any long running
+        methods.
+
+        Note: Since this is an async client, it won't block the UI even if you
+        call a long running method here but the UI updates (progress bars etc)
+        won't be available until the server completes running that method.
+        */
+        var request = {
+            kind        : 'call_instance_method',
+            id          : id,
+            method_name : method_name,
+            args        : this._marshal_all(args)
+        };
+
+        var deferred = new $.Deferred();
+        this.send_request(request).done(function(response){
+            deferred.resolve(this._unmarshal(response));
+        });
+
+        return deferred.promise();
+    };
+
+    jigna.AsyncClient.prototype.call_instance_method_thread = function(id, method_name, args) {
+        /* Calls an instance method in a thread on the server. Use this to call
+        any long running method on the server otherwise you won't get any UI
+        updates on the client.
+        */
+        var request = {
+            kind        : 'call_instance_method',
+            id          : id,
+            method_name : method_name,
+            args        : this._marshal_all(args),
+            thread      : true
+        };
+
+        // Note that this deferred is resolved when the method called in a thread
+        // finishes, not when the request to call the method finishes.
+        // This is done to make this similar to the sync client so that the users
+        // can attach their handlers when the method is done.
+        var deferred = new $.Deferred();
+
+        this.send_request(request).done(function(future_obj){
+
+            // the response of a threaded request is a marshalled version of a python
+            // future object. We attach 'done' and 'error' handlers on that object to
+            // resolve/reject our own deferred.
+
+            jigna.add_listener(future_obj, 'done', function(event){
+                deferred.resolve(event.data);
+            });
+
+            jigna.add_listener(future_obj, 'error', function(event){
+                deferred.reject(event.data);
+            });
+
+        });
+
+        return deferred.promise();
+    };
+
+    jigna.AsyncClient.prototype.get_attribute_from_server = function(proxy, attribute) {
+        /* Get the specified attribute of the proxy from the server. */
+
+        var request = this._create_request(proxy, attribute);
+
+        var response = this.send_request(request);
+        var result = this._unmarshal(response);
+
+        return result;
     };
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -1546,10 +1647,6 @@ define('main',['angular', 'jigna', 'jigna-angular'], function(angular, jigna){
     // Export the following namespaces to the global scope.
     window.angular = angular;
     window.jigna = jigna;
-
-    // Initialize jigna
-    jigna.initialize();
-
 });
 
 // NOTE: This is a fragment file which will be appended with
