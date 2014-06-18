d0e3cf2d540b35d2bcd42f9e3987eb9a45cde4df
Prashant Agrawal <prashant@enthought.com>
2014-06-18 13:31:38 +0530

    Clean up the user_resources example.

--- examples/user_resources.py
+++ examples/user_resources.py
@@ -1,27 +1,13 @@
 """
-This example shows how to add additional resources like CSS/Javascript/ image
-files in your html by specifying a base url.
+This example shows how to add additional resources like CSS, javascript
+and image files in your html by specifying a base url.
 """
 
 #### Imports ##################################################################
 
 from traits.api import HasTraits, Int, Str
+from pyface.qt import QtGui
 from jigna.api import View
-
-#### Utility function    ######################################################
-def parse_command_line_args(argv=None, description="Example"):
-    import argparse
-    parser = argparse.ArgumentParser(
-        description=description,
-        add_help=True
-        )
-    parser.add_argument("--web",
-                        help="Run the websocket version by starting a tornado server\
-                        on port 8888",
-                        action="store_true")
-    args = parser.parse_args(argv)
-    return args
-
 
 #### Domain model ####
 
@@ -33,47 +19,35 @@ class Person(HasTraits):
 
 body_html = """
     <div>
-        Name: <input ng-model="model.name">
-              <span class='red'>Always red - {{model.name}}</span>
-        Age:  <input ng-model="model.age" type='number'>
-              <span class='hoverme'>Hover me: {{model.age}}</span>
-        <img src='data/lena.png' />
+        <img ng-src='{{person.name}}.png' /><br/>
+        <div id="caption" class='red'>
+            {{person.name}} - {{person.age}} years old
+        </div>
     </div>
-    <script type='text/javascript' src='data/colorchange.js'></script>
-    <link rel='stylesheet' href='data/color.css' />
+
+    <script src='color_change.js'></script>
+    <link rel='stylesheet' href='colors.css' />
 """
 
-person_view = View(body_html=body_html, base_url='')
+# The base_url field specifies where to look when trying to get resources like
+# the css, js and image files (defaults to an empty string, i.e. the current
+# directory)
+person_view = View(body_html=body_html, base_url='user_resources_data/')
 
 #### Entry point ####
 
 def main():
-    def listener(obj, traitname, old, new):
-        print obj, traitname, old, new
+    # Start a QtGui application
+    app = QtGui.QApplication([])
 
-    fred  = Person(name='Fred', age=42)
-    fred.on_trait_change(listener)
-    def update_fred():
-        fred.name = "Wilma"
-        fred.age = 4
+    # Instantiate the domain model
+    lena = Person(name='Lena', age=28)
 
-    args = parse_command_line_args(description=__doc__)
-    if args.web:
-        import time
-        from threading import Thread
-        t = Thread(target=lambda:time.sleep(2) or update_fred())
-        t.daemon = True
-        t.start()
-        person_view.serve(model=fred)
-    else:
-        from pyface.qt import QtGui
-        from pyface.timer.api import do_after
-        app = QtGui.QApplication.instance() or QtGui.QApplication([])
-        person_view.show(model=fred)
-        do_after(2000, update_fred)
-        app.exec_()
-    print fred.name
-    print fred.age
+    # Render the view with the domain model added to the context
+    person_view.show(person=lena)
+
+    # Start the event loop
+    app.exec_()
 
 if __name__ == "__main__":
     main()
