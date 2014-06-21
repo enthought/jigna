79afde196fd3a3b56c43b51be6a0f40f6ec5dda7
Prashant Agrawal <prashant@enthought.com>
2014-06-21 13:58:54 +0530

    Slight cleanup of the dynamic_model_addition example.
    But I still feel that example is useless and we should drop it.

--- examples/dynamic_model_addition.py
+++ examples/dynamic_model_addition.py
@@ -1,12 +1,15 @@
 """
-This example shows how a model can be added dynamically to the scope.
+This example shows how a toplevel model can be added dynamically to the jigna
+view's context.
+
+Note: This example is currently for INTERNAL testing purposes only as I couldn't
+find a convincing use-case for this in real world applications.
 """
 
 #### Imports ####
 
 from traits.api import HasTraits, Int, Str
 from pyface.qt import QtGui
-from pyface.timer.api import do_after
 from jigna.api import View
 
 #### Domain model ####
@@ -19,17 +22,15 @@ class Person(HasTraits):
 
 body_html = """
     <div>
-      Model 1:  
-      Name: <input ng-model="model1.name">
-      Age: <input ng-model="model1.age" type='number'>
+      Person 1:
+      Name: <input ng-model="person1.name">
+      Age: <input ng-model="person1.age" type='number'>
 
       <br><br>
 
-      <div ng-if="model2">
-        Model 2: 
-        Name: <input ng-model="model2.name">
-        Age: <input ng-model="model2.age" type='number'>
-      </div>
+      Person 2:
+      Name: <input ng-model="person2.name">
+      Age: <input ng-model="person2.age" type='number'>
 
     </div>
 """
@@ -38,17 +39,25 @@ person_view = View(body_html=body_html)
 
 #### Entry point ####
 
-def add_model():
-    wilma = Person(name='Wilma', age=25)
-    person_view.update_context(model2=wilma)
-
 def main():
-    app = QtGui.QApplication.instance() or QtGui.QApplication([])
-    
-    fred = Person(name='Fred', age=42)
-    person_view.show(model1=fred)
-    do_after(2000, add_model)
+    # Start a QtGui application
+    app = QtGui.QApplication([])
 
+    # Instantiate the domain model
+    fred = Person(name='Fred', age=42)
+
+    # Render the view with the domain model added to the context
+    person_view.show(person1=fred)
+
+    # Add a new person to the context after some time
+    def add_person():
+        wilma = Person(name='Wilma', age=25)
+        person_view.update_context(person2=wilma)
+
+    from pyface.timer.api import do_after
+    do_after(2000, add_person)
+
+    # Start the event loop
     app.exec_()
 
 if __name__ == "__main__":
