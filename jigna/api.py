12e5ecc40e900a30f1226d9fdf3ad56e03a1bef2
Prashant Agrawal <prashant@enthought.com>
2014-06-30 16:42:24 +0530

    Move WebAppView to a file of its own.

--- jigna/api.py
+++ jigna/api.py
@@ -1,3 +1,4 @@
 from template import Template
 from qt_view import QtView
+from webapp_view import WebAppView
 from core.concurrent import Future
--- jigna/view.py
+++ jigna/view.py
@@ -7,17 +7,8 @@
 # This file is confidential and NOT open source.  Do not distribute.
 #
 
-
-# Standard library.
-import os
-from os.path import join
-
 # Enthought library.
-from traits.api import HasTraits, Instance, Dict, Int
-
-# Tornado Library
-from tornado.ioloop import IOLoop
-from tornado import web
+from traits.api import HasTraits, Instance, Dict
 
 # Jigna libary.
 from jigna.api import Template
@@ -47,47 +38,3 @@ class View(HasTraits):
 
     #: The server that manages the objects shared via the bridge.
     _server = Instance(Server)
-
-#### 'WebAppView' #############################################################
-
-class WebAppView(View):
-    """ A web based View to serve the jigna template of a given context. """
-
-    #### 'View' protocol ######################################################
-
-    def start(self):
-        """
-        Create the view's control and start the web server's event loop. This
-        is a *blocking* call.
-        """
-        ioloop = IOLoop.instance()
-
-        self.create_web_app()
-        self.web_app.listen(self.port)
-
-        ioloop.start()
-
-    #### 'WebAppView' protocol ################################################
-
-    web_app = Instance(web.Application)
-
-    port = Int(8000)
-
-    def create_web_app(self):
-        """ Create the web application serving the given context. Returns the
-        tornado application created. """
-
-        # Set up the WebServer to serve the domain models in context
-        from jigna.web_server import WebServer
-        self._server = WebServer(
-            base_url = join(os.getcwd(), self.template.base_url),
-            html     = self.template.html,
-            context  = self.context
-        )
-        self._server.initialize()
-
-        self.web_app = self._server.application
-
-        return self.web_app
-
-#### EOF ######################################################################
