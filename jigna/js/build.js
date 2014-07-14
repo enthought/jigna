d9a2395d7f5954311d4527225116b8334b8ad020
Prashant Agrawal <prashant@enthought.com>
2014-07-14 19:53:45 +0530

    Bring back the iframe solution.
    This somehow works perfectly now that we don't start a new tornado
    loop. We don't have to deal with the case of jquery messing with ipython
    either, neither do we have to worry about global scoping of the
    template's context.

--- jigna/js/build.js
+++ jigna/js/build.js
@@ -40,8 +40,8 @@ concatenate({
     base_url: 'src/',
     src: [
         // External dependencies
-        // 'external/jquery.min.js',
-        'external/angular.min.js',
+        'external/jquery.min.js',
+        'external/angular.js',
 
         // Internal dependencies
         'app/event_target.js',
@@ -63,7 +63,7 @@ concatenate({
     dest: 'dist/jigna.js',
     wrap: true,
     exports: [
-        // '$',
+        '$',
         'angular',
         'jigna'
     ]
--- jigna/utils/notebook.py
+++ jigna/utils/notebook.py
@@ -3,45 +3,25 @@ Utility methods for the IPython notebook
 """
 
 from jigna.api import WebApp
+from IPython.display import HTML
 
-call_count = 0
-app = None
+PORT = 8005
 
 def display_jigna(context, template):
     """
     A `display_html` style method to show rich jigna display for the objects
     within the context.
     """
-    global call_count, PORT, app
+    global PORT
 
-    call_count += 1
+    PORT += 1
 
-    PORT = 8005
+    app = WebApp(context=context, template=template, port=PORT)
+    application = app.create_application()
+    application.listen(app.port)
 
-    if not app:
-        app = WebApp(context=context, template=template, port=PORT)
-        application = app.create_application()
-        application.listen(app.port)
-    else:
-        app.update_context(context=context)
+    width, height = template.recommended_size
+    html = ('<iframe src="http://localhost:%s" width=%s height=%s></iframe>'
+            % (PORT, width, height))
 
-    from IPython.display import display_html, Javascript, HTML
-
-    # The Javascript code to compile the template which is added dynamically to
-    # the DOM
-    div_id = 'jigna-injected-' + str(call_count)
-    html = """
-        <div id='{div_id}'>
-            {html}
-        </div>
-    """.format(div_id=div_id, html=template.body_html)
-
-    display_html(html, raw=True)
-
-    js = """
-        window.jigna_server = "localhost:%s";
-        jigna.initialize({async: true});
-        jigna.angular.compile($("#" + %r)[0])
-    """ % (PORT, div_id)
-
-    return Javascript(js, lib='http://localhost:%s/jigna/jigna.js' % PORT)
+    return HTML(html)
