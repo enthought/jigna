559e2ce468bf0b521a77eb2eb9468ab702b1022e
Prashant Agrawal <prashant@enthought.com>
2014-07-14 21:01:30 +0530

    Add more sophisticated method to find a new port.

--- jigna/utils/notebook.py
+++ jigna/utils/notebook.py
@@ -5,16 +5,12 @@ Utility methods for the IPython notebook
 from jigna.api import WebApp
 from IPython.display import HTML
 
-PORT = 8005
-
 def display_jigna(context, template):
     """
     A `display_html` style method to show rich jigna display for the objects
     within the context.
     """
-    global PORT
-
-    PORT += 1
+    PORT = _get_free_port()
 
     app = WebApp(context=context, template=template, port=PORT)
     application = app.create_application()
@@ -25,3 +21,26 @@ def display_jigna(context, template):
             % (PORT, width, height))
 
     return HTML(html)
+
+def _get_free_port():
+    """
+    Return a free socket port. It works by creating an empty socket, binding it
+    to port 0 so that the OS automatically assigns a free port to it, obtaining
+    the port using `getsockname` and then immediately closing it.
+
+    The application intending to use this port should bind to it immediately so
+    that no other application binds to it before us.
+    """
+    import socket
+    sock = socket.socket()
+
+    # bind to a random port
+    sock.bind(('', 0))
+
+    # obtain the random port value
+    port = sock.getsockname()[1]
+
+    # close the socket so that the port gets free
+    sock.close()
+
+    return port
