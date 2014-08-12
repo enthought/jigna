b81d82c9f7aa38978755f8472a3f8b515957d27c
Prashant Agrawal <prashant@enthought.com>
2014-08-12 13:25:42 +0100

    Add a test to check if the jigna script is loaded in JS and we can access it.

--- jigna/tests/test_qt_app.py
+++ jigna/tests/test_qt_app.py
@@ -8,13 +8,13 @@ import unittest
 
 class TestQtApp(unittest.TestCase):
 
-    def test_widget_is_created(self):
-        # Create a QtApp with an empty template and empty context
-        template = Template(body_html="")
-        app = QtApp(template=template, context={})
+    def setUp(self):
+        self.template = Template(body_html="")
+        self.app = QtApp(template=self.template, context={})
 
+    def test_widget_is_created(self):
         # Create a widget
-        qwidget = app.create_widget()
+        qwidget = self.app.create_widget()
 
         # Check if a qwebview widget was created
         self.assertIsNotNone(qwidget)
@@ -22,18 +22,24 @@ class TestQtApp(unittest.TestCase):
         self.assertIsInstance(qwidget, QtWebKit.QWebView)
 
     def test_created_widget_loads_html(self):
-        # Create a QtApp with an empty template and empty context
-        template = Template(body_html="")
-        app = QtApp(template=template, context={})
-
         # Create a widget
-        qwidget = app.create_widget()
+        qwidget = self.app.create_widget()
         GUI.process_events()
 
         # Check if the widget has html loaded in it
         frame = qwidget.page().mainFrame()
         self.assertIsInstance(frame.toHtml(), basestring)
         self.assertGreater(len(frame.toHtml()), 0)
+
+    def test_created_widget_can_access_jigna(self):
+        # Create a widget and execute a js to determine whether jigna is
+        # available in the Javascript console or not
+        qwidget = self.app.create_widget()
+        GUI.process_events()
+
+        # Check if the widget was created
+        jigna = self.app.execute_js("jigna");
+        self.assertIsNotNone(jigna)
 
 if __name__ == "__main__":
     unittest.main()
