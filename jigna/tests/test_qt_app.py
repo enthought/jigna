31d79e035e25906edc4dae2dd6fcd50a9592be6b
Prashant Agrawal <prashant@enthought.com>
2014-08-12 13:23:00 +0100

    Add another test to check if jigna widget can load html or not.

--- jigna/tests/test_qt_app.py
+++ jigna/tests/test_qt_app.py
@@ -1,6 +1,9 @@
 from jigna.api import QtApp
 from jigna.api import Template
 
+from pyface.qt import QtWebKit, QtGui
+from pyface.gui import GUI
+
 import unittest
 
 class TestQtApp(unittest.TestCase):
@@ -11,11 +14,26 @@ class TestQtApp(unittest.TestCase):
         app = QtApp(template=template, context={})
 
         # Create a widget
-        widget = app.create_widget()
+        qwidget = app.create_widget()
 
-        # Check if the widget was created
-        self.assertIsNotNone(widget)
+        # Check if a qwebview widget was created
+        self.assertIsNotNone(qwidget)
+        self.assertIsInstance(qwidget, QtGui.QWidget)
+        self.assertIsInstance(qwidget, QtWebKit.QWebView)
 
+    def test_created_widget_loads_html(self):
+        # Create a QtApp with an empty template and empty context
+        template = Template(body_html="")
+        app = QtApp(template=template, context={})
+
+        # Create a widget
+        qwidget = app.create_widget()
+        GUI.process_events()
+
+        # Check if the widget has html loaded in it
+        frame = qwidget.page().mainFrame()
+        self.assertIsInstance(frame.toHtml(), basestring)
+        self.assertGreater(len(frame.toHtml()), 0)
 
 if __name__ == "__main__":
     unittest.main()
