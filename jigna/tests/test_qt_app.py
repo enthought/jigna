7dd4c707761701b6bb9e47f749a79d04d099fe59
Prashant Agrawal <prashant@enthought.com>
2014-08-13 12:51:03 +0100

    Remove `GUI.process_events` call from the tests as the widget is always ready.

--- jigna/tests/test_qt_app.py
+++ jigna/tests/test_qt_app.py
@@ -2,7 +2,6 @@ from jigna.api import QtApp
 from jigna.api import Template
 
 from pyface.qt import QtWebKit, QtGui
-from pyface.gui import GUI
 
 from traits.api import HasTraits, Str, Int
 
@@ -31,7 +30,6 @@ class TestQtApp(unittest.TestCase):
     def test_created_widget_loads_html(self):
         # Create a widget
         qwidget = self.app.create_widget()
-        GUI.process_events()
 
         # Check if the widget has html loaded in it
         frame = qwidget.page().mainFrame()
@@ -41,7 +39,6 @@ class TestQtApp(unittest.TestCase):
     def test_created_widget_can_access_jigna(self):
         # Create a widget
         qwidget = self.app.create_widget()
-        GUI.process_events()
 
         # Check if `jigna` was loaded on the JS side
         jigna = self.app.execute_js("jigna");
@@ -50,7 +47,6 @@ class TestQtApp(unittest.TestCase):
     def test_jigna_was_initialized(self):
         # Create a widget
         qwidget = self.app.create_widget()
-        GUI.process_events()
 
         # Check if `jigna` was initialised on the JS side
         client = self.app.execute_js("jigna.client");
@@ -59,7 +55,6 @@ class TestQtApp(unittest.TestCase):
     def test_jigna_was_initialized_with_python_models(self):
         # Create a widget
         qwidget = self.app.create_widget()
-        GUI.process_events()
 
         # Check if `jigna` was initialised with the correct python models
         # (check by making sure that primitive variables are the same)
