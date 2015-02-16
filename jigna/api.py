6d2c1d9087fd94a9c94f6c0875f391ae5898fd6a
Prashant Agrawal <prashant@enthought.com>
2015-02-16 19:35:07 +0530

    Add a comment for wrapping WebApp import within a try-catch block

--- jigna/api.py
+++ jigna/api.py
@@ -1,6 +1,9 @@
 from template import Template
 from core.concurrent import Future
 from qt_app import QtApp
+
+# Wrapping the WebApp import so that you can use jigna even if you don't have
+# tornado install
 try:
     from web_app import WebApp
 except ImportError:
