428f79b828707def96abc9d74d4952b3ac6198cc
Martin Chilvers <martin.chilvers@gmail.com>
2015-04-15 12:13:20 +0100

    Made private attributes private :)

--- jigna/html_widget.py
+++ jigna/html_widget.py
@@ -4,54 +4,57 @@
 # (C) Copyright 2013 Enthought, Inc., Austin, TX
 # All right reserved.
 #
+""" A Qt based HTML widget to render a jigna UI. """
+
 
 from __future__ import absolute_import
 
-# Standard Library
+# Standard library.
 import os
 from os.path import join
 
-# Local Library
+# Local library.
 from .qt import QtGui
 from .qt_server import QtServer
 
 
 class HTMLWidget(QtGui.QWidget):
-    """ A Qt based HTML widget to render the jigna template with a given
-    domain model context. """
+    """ A Qt based HTML widget to render a jigna UI. """
 
+    #### 'object' protocol #####################################################
+    
     def __init__(
         self, parent=None, window_flags=0, context=None, template=None
     ):
+        """ Constructor. """
+        
         super(HTMLWidget, self).__init__(parent, window_flags)
 
-        self.context = context
-        self.template = template
+        # Private protocol.
+        self._context  = context
+        self._template = template
+        self._server   = self._create_server()
 
-        self.webview = self._create_qwebview()
-
-        # Set the layout
+        # Set the widget layout.
         self.setLayout(QtGui.QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
-        self.layout().addWidget(self.webview)
+        self.layout().addWidget(self._server.webview)
         self.resize(*template.recommended_size)
 
     def execute_js(self, js):
-        """ Execute the given js string on the HTML widget.
-        """
-        return self.webview.execute_js(js)
+        """ Execute the given JS string in the HTML widget. """
+
+        return self._server.webview.execute_js(js)
 
     #### Private protocol #####################################################
 
-    def _create_qwebview(self):
-        """ Create a qwebview to render the template with the context.
-        """
+    def _create_server(self):
+        """ Create a jigna Qt server to serve the domain models. """
 
-        # Set up the server to serve the domain models in context
         server = QtServer(
-            base_url = join(os.getcwd(), self.template.base_url),
-            html     = self.template.html,
-            context  = self.context
+            base_url = join(os.getcwd(), self._template.base_url),
+            html     = self._template.html,
+            context  = self._context
         )
 
-        return server.webview
+        return server
